ffa2e2ddd467f6ec32a5cac208c06982a597bae2
trungnguyen <quangtrung01654748714@gmail.com>
2024-08-09 18:42:14 +0700

    up

--- assets/Scripts/UserInfoCtr.ts
+++ assets/Scripts/UserInfoCtr.ts
@@ -1,16 +1,37 @@
-import { _decorator, Component, Node } from 'cc';
+import { _decorator, Component, director, Label, Node } from 'cc';
 import { HomeController } from './HomeController';
 import { UserDataStore } from './UserDataStore';
 const { ccclass, property } = _decorator;
 
 @ccclass('UserInfoCtr')
 export class UserInfoCtr extends Component {
+    @property({type: Label})
+    username: Label;
+    @property({type: Label})
+    account: Label;
+    @property({type: Label})
+    accountType: Label;
+    @property({type: Label})
+    password: Label;
+    @property({type: Label})
+    email: Label;
+    @property({type: Label})
+    phone: Label;
     start() {
-        console.log("dataUserInfo: " + JSON.stringify(UserDataStore.instance.getData()));
+        this.username.string = UserDataStore.instance.data.username;
+        this.accountType.string = UserDataStore.instance.data.account_type;
+        this.account.string = UserDataStore.instance.data.account;
+        this.password.string = UserDataStore.instance.data.password;
+        this.email.string = UserDataStore.instance.data.email;
+        this.phone.string = UserDataStore.instance.data.phone;
     }
 
     update(deltaTime: number) {
         
+    }
+    btnClose()
+    {
+        director.loadScene("scene");
     }
 }
 
