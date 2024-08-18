886b6f313a5c46e1d62db37b7b29e12b7f14e88b
dev_trung <quangtrung01654748714@gmail.com>
2024-08-18 20:13:08 +0700

    up

--- assets/Scripts/HomeController.ts
+++ assets/Scripts/HomeController.ts
@@ -189,6 +189,37 @@ export class HomeController extends Component {
     openURLWin(url: string): void {
          window.open(url, '_blank');
     }
+    btnNextSupport()
+    {
+        let url = "https://t.me/ft88cskh";
+        if(sys.isBrowser)
+        {
+            window.open(url, '_blank');
+
+        }
+        else {
+            window.location.href = url;
+        }
+        // if (sys.isBrowser) {
+            // window.open(url, '_blank');
+        // } else if (sys.isNative) {
+        //     if (sys.platform === sys.Platform.ANDROID) {
+        //         // console.log("Android");
+        //         // native.reflection.callStaticMethod("com/cocos/game/AppActivity", "openURL", "(Ljava/lang/String;)V", "url");
+        //     } else if (sys.platform === sys.Platform.IOS) {
+        //         // console.log("IOS");
+        //         // native.reflection.callStaticMethod("AppController", "openURL:", url);
+        //         // native.reflection.callStaticMethod("com/cocos/game/AppActivity", "openURL", "(Ljava/lang/String;)V", "https://example.com");
+
+        //     }
+        // }
+        // if (sys.os === "Android") {
+        //     window.location.href = url;
+
+        // } else if (sys.os === "iOS") {
+        //     window.location.href = "https://www.winwin999.net/DownloadPage/AppDownload_M?site=viva88&platform=ios&lang=en";
+        // } 
+    }
    
 }
 
